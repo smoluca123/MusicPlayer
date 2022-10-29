190d35385a6e6e2f47966794d8b797ff634454a0
Luca Nguyen <lluca8257@gmail.com>
2022-10-29 22:12:22 +0700

    Finish

--- assets/js/app.js
+++ assets/js/app.js
@@ -3,6 +3,8 @@ import songs from './listSongs.js';
 var $ = document.querySelector.bind(document);
 var $$ = document.querySelectorAll.bind(document);
 
+const PLAYER_STORAGE_KEY = 'MusicPlayer';
+
 const player = $('.player');
 const heading = $('header h2');
 const cdThumb = $('.cd-thumb');
@@ -13,16 +15,29 @@ const progress = $('#progress');
 const muteBtn = $('.btn-speaker');
 const nextSongBtn = $('.btn-next');
 const prevSongBtn = $('.btn-prev');
+const randomBtn = $('.btn-random');
+const repeatBtn = $('.btn-repeat');
+const playlist = $('.playlist');
+const backwardBtn = $('.btn-backward');
 
 const app = {
   currentIndex: 0,
   isPlaying: false,
   isMuted: false,
+  isRandom: false,
+  isRepeat: false,
+  config: JSON.parse(localStorage.getItem(PLAYER_STORAGE_KEY)) || {},
   songs: songs,
+  setConfig: function (key, value) {
+    this.config[key] = value;
+    localStorage.setItem(PLAYER_STORAGE_KEY, JSON.stringify(this.config));
+  },
   render: function () {
-    const htmls = this.songs.map((song) => {
+    const htmls = this.songs.map((song, index) => {
       return `
-        <div class="song">
+        <div class="song ${
+          index === this.currentIndex ? 'active' : ''
+        }" data-index="${index}">
             <div class="thumb" style="background-image: url('${song.image}')">
             </div>
             <div class="body">
@@ -35,7 +50,7 @@ const app = {
         </div>
         `;
     });
-    $('.playlist').innerHTML = htmls.join('');
+    playlist.innerHTML = htmls.join('');
   },
   defineProperties: function () {
     Object.defineProperty(this, 'currentSong', {
@@ -97,6 +112,9 @@ const app = {
       }
       if (e.key == 'm' || e.code == 'KeyM') {
         _this.muteSong();
+        _this.isMuted = !_this.isMuted;
+        _this.setConfig('isMuted', _this.isMuted);
+        player.classList.toggle('muted', _this.isMuted);
       }
       if (e.code == 'ArrowRight') {
         _this.rewind(5);
@@ -112,10 +130,6 @@ const app = {
         e.preventDefault();
         _this.volumeChange(-0.1);
       }
-      return;
-    });
-    muteBtn.addEventListener('click', function () {
-      _this.muteSong();
     });
 
     // xử lí duration - tiến trình hiện tại của audio
@@ -137,19 +151,97 @@ const app = {
     // xử lí next track
     nextSongBtn.addEventListener('click', function () {
       _this.isPlaying = false;
-      _this.nextSong();
+      if (_this.isRandom) {
+        _this.playRandomSong();
+      } else {
+        _this.nextSong();
+      }
       audio.play();
+      _this.render();
+      _this.scrollToView();
     });
+    // xử lí prev track
     prevSongBtn.addEventListener('click', function () {
       _this.isPlaying = false;
-      _this.prevSong();
+      if (_this.isRandom) {
+        _this.playRandomSong();
+      } else {
+        _this.prevSong();
+      }
+      audio.play();
+      _this.render();
+      _this.scrollToView();
+    });
+
+    // Xử lí random track
+    randomBtn.addEventListener('click', function () {
+      _this.isRandom = !_this.isRandom;
+      _this.setConfig('isRandom', _this.isRandom);
+      randomBtn.classList.toggle('active', _this.isRandom);
+    });
+    // Xử lí repeat track
+    repeatBtn.addEventListener('click', function () {
+      _this.isRepeat = !_this.isRepeat;
+      _this.setConfig('isRepeat', _this.isRepeat);
+      repeatBtn.classList.toggle('active', _this.isRepeat);
+    });
+    // xử lí tắt mở âm thanh
+    muteBtn.addEventListener('click', function () {
+      _this.muteSong();
+      _this.isMuted = !_this.isMuted;
+      _this.setConfig('isMuted', _this.isMuted);
+      player.classList.toggle('muted', _this.isMuted);
+    });
+    backwardBtn.addEventListener('click', function () {
+      _this.currentIndex = 0;
+      _this.loadCurrentSong();
+      _this.render();
       audio.play();
     });
+
+    //Xử lí next track khi ended
+    audio.onended = function () {
+      if (_this.isRepeat) {
+        audio.play();
+      } else {
+        nextSongBtn.click();
+      }
+    };
+
+    // lắng nghe action click vào playlist
+    playlist.addEventListener('click', function (e) {
+      const songNode = e.target.closest('.song:not(.active)');
+      if (songNode || e.target.closest('.option')) {
+        // Xử lí khi click vào song
+        if (songNode) {
+          songNode.classList.toggle('active');
+          _this.currentIndex = Number(songNode.dataset.index);
+          _this.loadCurrentSong();
+          _this.render();
+          audio.play();
+        }
+        // Xử lí khi click vào option
+        if (e.target.closest('.option')) {
+        }
+      }
+    });
+  },
+  scrollToView: function () {
+    setTimeout(() => {
+      $('.song.active').scrollIntoView({
+        behavior: 'smooth',
+        block: 'center',
+      });
+    }, 300);
   },
   loadCurrentSong: function () {
     heading.innerText = this.currentSong.name;
     cdThumb.style.backgroundImage = `url('${this.currentSong.image}')`;
     audio.src = this.currentSong.path;
+  },
+  loadConfig: function () {
+    this.isRandom = this.config.isRandom;
+    this.isRepeat = this.config.isRepeat;
   },
   nextSong: function () {
     this.currentIndex++;
@@ -160,15 +252,24 @@ const app = {
   },
   prevSong: function () {
     this.currentIndex--;
-    if (this.currentIndex <= 0) {
-      this.currentIndex = 0;
+    if (this.currentIndex < 0) {
+      this.currentIndex = this.songs.length - 1;
     }
     this.loadCurrentSong();
   },
+  playRandomSong: function () {
+    let newIndex;
+    do {
+      newIndex = Math.floor(Math.random() * this.songs.length);
+    } while (this.currentIndex === newIndex);
+    this.currentIndex = newIndex;
+    this.loadCurrentSong();
+  },
+  playRepeatSong: function () {
+    this.loadCurrentSong();
+  },
   muteSong: function () {
-    player.classList.toggle('muted');
     audio.muted = !this.isMuted;
-    this.isMuted = !this.isMuted;
   },
   rewind: function (second) {
     audio.currentTime = audio.currentTime + second;
@@ -191,12 +292,17 @@ const app = {
   },
 
   start: function () {
+    // Gán cấu hình từ config vào ứng dụng
+    this.loadConfig();
     this.defineProperties();
     this.handleEvents();
 
     this.loadCurrentSong();
 
     this.render();
+
+    repeatBtn.classList.toggle('active', this.isRepeat);
+    randomBtn.classList.toggle('active', this.isRandom);
   },
 };
 app.start();
